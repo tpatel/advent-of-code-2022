3539e361442cd3d9cc0bbf717501c7e036cd48d9
Thibaut Patel <thibaut@ghost.org>
2022-12-12 09:35:08 +0100

    Updated the template following a mistake I made in day 11 (part 2)
    This avoids side-effects between part 1 and 2. In day 11, I did modify the input in part 1, which affected part2. I was hard to debug, hence this change!

--- template.mjs
+++ template.mjs
@@ -3,14 +3,20 @@ import { readFileSync } from "node:fs";
 const lines = readFileSync("day??.txt", { encoding: "utf-8" }) // read day??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
-  .split("\n") // Split on newline
-  .map(Number); // Parse each line into a number
+  .split("\n"); // Split on newline
+
+// Return a new object to avoid side effects between part 1 and 2
+function getInput() {
+  return [...lines];
+}
 
 function part1() {
+  const input = getInput();
   //do something here
 }
 
-function part2() {
+function getInput() {
+  const input = getInput();
   //do something here
 }
 
